78b51ed7bc3a2615f68cfad455d6fddf611f5029
dlb213 <dlb213@lehigh.edu>
2019-01-16 16:03:23 -0500

    updated the script to be ready to use.  didn't test it yet. Leaving for the day so I need to make a commit

--- updatesite.sh
+++ updatesite.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#do a check to make sure both of these exists
 site=$1
 name=$2
 
@@ -7,32 +8,41 @@ cd /var/www/drupal/sites/$site
 
 #update user 1
 uid1='cas_$name_admin'
-drush sqlq 'update users set name="$uid1" where uid=1';
+drush sqlq 'update users set name="$uid1",mail="incasweb@lehigh.edu" where uid=1';
 
-drush upwd --password="$(pwgen -s 32)" castopadmin;
+drush upwd --password="$(pwgen -s 32)" "$uid1"
+
+#set site mail
+drush vset site_mail "incasweb@lehigh.edu"
 
 #remove people
 drush ucan ark218,gsl217,rbc218,ena219
 
 #should I remove admin from everyone for now? probably
 
-#disable modules
+#### Disable modules
 
 #disable, don't uninstall:
-drush dis comments,tracker
+drush dis -y comments,tracker
 
 #disable, then uninstall
-$dis="overlay,panels,dashboard,color,colors,imce_mkdir"
-$dis1="fullcalendar_colors"
+$dis="overlay,dashboard,color,imce_mkdir"
+#$dis1="fullcalendar_colors"
 
-drush dis "$dispmu"
+drush dis -y "$dis"
+#drush dis -y "$dis1"
 
 #uninstall the disabled modules
-drush pmu "$dis"','"$dis1"
+#drush pmu -y "$dis"','"$dis1"
+drush pmu -y "$dis"
 
-#don't uninstall
-#      comments,tracker
-
+### remove modules that aren't on the server
+#if the modules folder exists
+cp -r ~/UNINSTALLmodules ./modules
+drush dis -y oauth_common,panels,colors
+drush pmu -y oauth_common,panels,colors
+rm -r ./modules/UNINSTALLmodules
+#I'm sure there will be more to add to the above, maybe if enough get added
 
 #maybe add something in here displaying other modules that should be uninstalled.
 
